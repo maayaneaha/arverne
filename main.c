03189e550056195a0c9359f331519bdc09bc5e10
coni <46821134+coni@users.noreply.github.com>
2023-02-13 21:51:27 +0100

    simple

--- main.c
+++ main.c
@@ -14,17 +14,22 @@ int main(int argc, char* argv[])
     //coni();
     return 0;
 #else
-    ArgOpt argopt = getopt_Parse(argc, argv);
+    /* ArgOpt argopt = getopt_Parse(argc, argv); */
     
     double deltav = 0;
     double twrmin = 0;
     double twrmax = 0;
-    if (argopt.deltav)
-        deltav = atof(argopt.deltav);
-    else if (argopt.twrmin)
-        twrmin = atof(argopt.twrmin);
-    else if (argopt.twrmax)
-        twrmax = atof(argopt.twrmax);
+    if (argc > 1)
+        deltav = atof(argv[1]);
+    if (argc > 2)
+    {
+        twrmin = atof(argv[2]);
+    }
+    if (argc > 3)
+    { 
+        twrmax = atof(argv[3]);
+    }
+    printf("%F\n%F\n%F", deltav, twrmin, twrmax);
 
     return 0;
 #endif
