ad0c903a4cab98ceec0bdab05dcc69fc771a326f
oscar <oscar.m.chevalier@gmail.com>
2023-03-23 16:01:52 +0100

    algo2: initialisation of algopti.c

---
+++ src/algopti.c
@@ -0,0 +1,4 @@
+// on itere le nombre d'etage de 0 a 100
+// on cherche a repartir equitablement de dv
+// pour choisir un moteur, on prend celui qui permet d'avoir le plus de moteurs
+// si le dv n'est pas satisfait, on le reparti sur les autres etages
