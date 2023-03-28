00402e33efbda846ee60a4a8d56519da7af55445
oscar <oscar.m.chevalier@gmail.com>
2023-03-28 15:23:10 +0200

    algo2: search_engine finished

--- include/algo/algopti.h
+++ include/algo/algopti.h
@@ -2,6 +2,7 @@
 #define ARVERNE_ALGOPTI_H
 
 #include "utils.h"
+#include "physics.h"
 int linear_algo(Datas* d);
 
 #endif //ARVERNE_ALGOPTI_H
--- src/algo/algopti.c
+++ src/algo/algopti.c
@@ -8,6 +8,28 @@
 // utiliser calculate_mass_fuel pour trouver la masse totale et prendre la plus faible
 Engine* search_engine(Datas* d, Rocket* r, double dv_needed, int* nbr_engines)
 {
+    Engine* optimal_engine = NULL;
+    double minimal_mass = INF;
+    for(size_t i = 0; i < nbr_engines; i++)
+    {
+        int nr_engines = nbr_engines_needed();
+        Engine* e = d->engines[i];
+        //double calculate_mass_fuel(double deltaV, int ISP, double g, beta, double m_engines, double m_payload)
+        double mass_fuel = calculate_mass_fuel(dv_needed, calculate_isp(optimal_engine),
+                                               calculate_g(), beta, nbr_engines,
+                                               r->total_mass);
+        if (mass_fuel < 0)
+        {
+            continue;
+        }
+        double mass_total = r->total_mass mass_fuel + e->mass * nbr_engines;
+        if (mass_total < minimal_mass)
+        {
+            optimal_engine = e;
+            minimal_mass = mass_total;
+        }
+    }
+    return optimal_engine;
 }
 
 
@@ -26,7 +48,7 @@ int search_stage(Datas* d, Rocket* r, double dv_needed)
 int search_rocket(Datas* d, size_t nbr_stages)
 {
     Rocket *r = create_rocket(datas);
-    r->cost = 999999999;
+    r->cost = INF;
     int ret = 0;
     for (size_t i = nbr_stages; i > 0; i--)
     {
