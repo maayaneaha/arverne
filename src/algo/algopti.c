95dbb7eba9f292632e2524e5c2d3cde7ce6db93c
oscar <oscar.m.chevalier@gmail.com>
2023-03-28 13:59:33 +0200

    algo2: search_stage completed

--- src/algo/algopti.c
+++ src/algo/algopti.c
@@ -6,15 +6,21 @@
 // si le dv n'est pas satisfait, on le reparti sur les autres etages
 
 // utiliser calculate_mass_fuel pour trouver la masse totale et prendre la plus faible
-Engine search_engine()
+Engine* search_engine(Datas* d, Rocket* r, double dv_needed, int* nbr_engines)
 {
-    
 }
 
 
-double search_stage(Datas* d, Rocket* r, double dv_needed)
+int search_stage(Datas* d, Rocket* r, double dv_needed)
 {
-    
+    int nbr_engines;
+    Engine* optimal_engine = search_engine(d, r, dv_needed, &nbr_engines);
+    // int create_tank_stack(Datas *d, Stage *s, enum diameter diam, double mass_fuel)
+    Stage* s = create_stage(d);
+    s->engine = create_engine(optiam_engine);
+    s->nbr_engines = nbr_engines;
+    append_stage(r, s);
+    return 1;
 }
 
 int search_rocket(Datas* d, size_t nbr_stages)
@@ -24,11 +30,12 @@ int search_rocket(Datas* d, size_t nbr_stages)
     int ret = 0;
     for (size_t i = nbr_stages; i > 0; i--)
     {
-        if ((int dv_remaining = search_stage(d, r, d->deltaV_min / i)) != 0)
+        if (!search_stage(d, r, d->deltaV_min / i))
         { // The stage can't have enough DV, we share it between the other stages
 #if DEBUG
-            printf("search_rcoket: dv_remaining = %f\n", dv_remaining);
+            printf("search_rocket: search_stage failed\n");
 #endif
+            return 0;
         }
     }
     return ret;
