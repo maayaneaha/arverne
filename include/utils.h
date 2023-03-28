578af110b8edaf5ec24778a076b6240d5034919d
oscar <oscar.m.chevalier@gmail.com>
2023-03-28 14:54:48 +0200

    algo2: calculate_isp, nbr_engine and calculate_mass_fuel added or modified.

--- include/utils.h
+++ include/utils.h
@@ -130,6 +130,8 @@ int create_tank_stack(Datas *d, Stage *s, enum diameter diam, double mass_fuel);
 int append_stage(Rocket *r, Stage *s);
 
 #define NBR_SEARCH_STAGES 10 // number of stages for the algopti search
+#define INF 999999999
+#define BETA 0.1
 
 #endif
 
