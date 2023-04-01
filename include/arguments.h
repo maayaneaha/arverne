744e980501eaafd83e93a41472be9ddbca1ba368
coni <46821134+coni@users.noreply.github.com>
2023-04-01 22:57:51 +0000

    tutorial: how to add parameters

--- include/arguments.h
+++ include/arguments.h
@@ -1,12 +1,16 @@
 #pragma once
+
 #include <stddef.h>
 
 struct ArgOpt_s {
     // char* == type 1 (with value)
     // int == type 2 (flag)
-    char* deltav;
-    char* twrmin;
-    char* twrmax;
+    char* version;
+    char* username;
+    int skip_assets;
+    int login_microsoft;
+    int list_installed;
+    int help;
 }; 
 
 typedef struct ArgOpt_s ArgOpt;
@@ -17,8 +21,9 @@ typedef struct {
     char* longname;
     int type; // 0 == "with value"; 1 == "flag"
     int required;
-    char* desc;
+    char* description;
 } Opt;
 
 ArgOpt getopt_Parse(int argc, char* argv[]);
 void getopt_CheckRequired(ArgOpt argopt);
+void getopt_Help();
--- src/arguments.c
+++ src/arguments.c
@@ -1,16 +1,33 @@
 #include "arguments.h"
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <err.h>
 
-ArgOpt argopt = {NULL, NULL, NULL};
-Opt options[] = {
-   /* value                     shortname   longname        type    required    description */
-    { &argopt.deltav,          "d",    "deltav",          0,      1,      "deltav" },
-    { &argopt.twrmin,         "m",    "twrmin",         0,      0,      "twr min" },
-    { &argopt.twrmax,      "M",   "twrmax",      1,      0,      "twr max" },
-};
+/* Petit guide de comment on utillise les paramètres 
+ * 
+ * 1ere étape : ajoutes des éléments dans options[] juste en bas
+ *	pour donner un exemple avec le parametre version 
+ *	--version [-v] sont deux moyens de spécifier le paramètres
+ *	Dans section "type" 0 c'est pour dire qu'il prend un parametre
+ *	et si le "type" est a 1, cela veut dire qu'il ne prend pas de parametres
+ *	required c'est pour déterminer si le parametre est nécessaire au lancement
+ *	du logiciel
+ *	Description c'est pour donner une idée de la commande
+ *	les description sont censé allé dans la commande --help [-h]
+ *
+ *	2eme étape : dans arguments.h
+ *	 ajoutes des variables dans la struct ArgOpt_s
+ *	 la struct prend soit des int soit des char*
+ *	 le int c'est pour dire que la variable de prend pas de parametres
+ *	 le char* la variable prend un parametre
+ *	 si tu par exemple dans un parametre tu as un nombre tu devras
+ *	 le convertir par la suite manuellement
+ *	 mes fonctions convertissent pas les parametres en nombre
+ *
+ *	 et normalement c'est tout hehe
+*/
 
 char* str_cpyrange(char* src, int start, int size)
 {
@@ -26,20 +43,43 @@ char* str_cpyrange(char* src, int start, int size)
     return out;
 }
 
-/* void getopt_CheckRequired(ArgOpt argopt) */
-/* { */
-/*     Opt* ptr = argopt; */
-/*     Opt* end_ptr = options + sizeof(options)/sizeof(options[0]); */
-/*     for (ptr = options; ptr < end_ptr; ptr++) */
-/*     { */
-/*         char** tmp_val = ptr->value; */
-/*         if (ptr->required && *tmp_val == NULL) */
-/*         { */
-/*             printf("MISSING REQUIRED PARAMETERS --%s [-%s]", ptr->longname, ptr->shortname); */
-/*             errx(1, "MISSING REQUIRED PARAMETERS --%s [-%s]", ptr->longname, ptr->shortname); */
-/*         } */
-/*     } */
-/* } */
+ArgOpt argopt = {NULL, NULL, 0, 0, 0, 0};
+Opt options[] = {
+	 /*exemple avec mon launcher mincraft*/
+	/*value                     shortname   longname        type    required    description */
+	/*{ &argopt.version,          "v",    "version",          0,      1,      "Specify the version of the game" },*/
+	/*{ &argopt.username,         "u",    "username",         0,      0,      "Specify the username in-game" },*/
+	/*{ &argopt.skip_assets,      "sa",   "skip-assets",      1,      0,      "Don't download assets" },*/
+	/*{ &argopt.login_microsoft,  "lm",   "login-microsoft",  1,      0,      "Login into a Microsoft Account" },*/
+	/*{ &argopt.list_installed,   "li",   "list-installed",   1,      0,      "Show installed version of Minecraft" },*/
+	/*{ &argopt.help,   "h",   "help",   1,      0,      "Show all the parameters of the launcher" },*/
+	NULL
+};
+size_t options_len = 6;
+
+void getopt_Help()
+{
+    Opt* ptr = NULL;
+    for (size_t i = 0; i < options_len; i++)
+    {
+        ptr = options + i;
+        printf("%s      %s\n", ptr->longname, ptr->description);
+    }
+}
+
+void getopt_CheckRequired(ArgOpt argopt)
+{
+    Opt* ptr = NULL;
+    Opt* end_ptr = options + sizeof(options)/sizeof(options[0]);
+    for (ptr = options; ptr < end_ptr; ptr++)
+    {
+        char** tmp_val = ptr->value;
+        if (ptr->required && *tmp_val == NULL)
+        {
+            errx(1, "MISSING REQUIRED PARAMETERS --%s [-%s]", ptr->longname, ptr->shortname);
+        }
+    }
+}
 
 ArgOpt getopt_Parse(int argc, char* argv[])
 {
@@ -77,8 +117,7 @@ ArgOpt getopt_Parse(int argc, char* argv[])
             option_name = str_cpyrange(argv[i], flag_type, delimiter-flag_type);
             for (ptr = options; ptr < end_ptr && unknown_type == 1; ptr++)
             { 
-                if ((flag_type == 1 && strcmp(ptr->shortname, option_name)) == 0 || 
-                        (flag_type == 2 && strcmp(ptr->longname, option_name)) == 0)
+                if ((flag_type == 1 && strcmp(ptr->shortname, option_name) == 0) || (flag_type == 2 && strcmp(ptr->longname, option_name) == 0))
                 {
                     unknown_type = 0;
                     // COMPARER LES SHORTNAME ET LONGNAME
